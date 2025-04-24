d21032fccd24b4509dc3b6c6a65eac883895c202
Sohel Sheikh <sohelxsheikh@gmail.com>
2025-04-24 13:51:36 +0530

    deployment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-anton text-">
+    <div className="font-anton text-orange text-center text-7xl">
        Hey KindMeal.
     </div>  
   );
