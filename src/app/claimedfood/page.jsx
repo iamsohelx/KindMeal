987c1a2aabb49c8d6fb32a88eb3f4d01c72ec6b5
Sohel Sheikh <sohelxsheikh@gmail.com>
2025-05-26 20:29:16 +0530

    FoodPass

--- src/app/claimedfood/page.jsx
+++ src/app/claimedfood/page.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import { Suspense } from 'react'
 import { FoodPass } from '../components/FoodPass'
+import { cn } from "@/lib/utils";
+
+import Navbar from '../components/Navbar'
 
 const Page = () => {
   return (
     
-      <Suspense>
+      
+        <>
+    <Navbar/>
+    <div>
+    <div>
+       <div className="relative flex h-screen w-screen items-center justify-center bg-gray-200 dark:bg-black">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:20px_20px]",
+          "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+          "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
+        )}
+      />
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+           <div className='w-full md:w-[50%] flex justify-center relative'>
+               <Suspense>
         <FoodPass/>
       </Suspense>
+           </div>
+    </div>
+    </div>
+    </div>
+</>
   )
 }
 
